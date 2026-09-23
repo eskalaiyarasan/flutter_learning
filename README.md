87aef26f9ceecb1c4c189dd8391a3a648dc7b50f
eskalaiyarasan@gmail.com <eskalaiyarasan@gmail.com>
2026-09-23 09:02:43 +0530

    intermittent----

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # flutter_learning
+/!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+lib/main.dart:15:17: Error: Cannot invoke a non-'const' constructor where a const expression is expected.
+Try using a constructor or factory that is 'const'.
+        appBar: AppBar(
+==================================================
+Remove const before metrialApp
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!/
+
--- birdle/lib/main.dart
+++ birdle/lib/main.dart
@@ -2,25 +2,24 @@ import 'package:flutter/material.dart';
 import 'game.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+   MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Tile('A', HitType.hit),
-              Tile('A', HitType.miss),
-              Tile('A', HitType.partial),
-            ],
+        appBar: AppBar(
+          title: Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Birdle'),
           ),
+        ),
+        body: Center(
+          child: GamePage()
         ),
       ),
     );
@@ -53,6 +52,81 @@ class Tile extends StatelessWidget {
           style: Theme.of(context).textTheme.titleLarge,
         ),
       ),
+    );
+  }
+}
+
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+  // This object is part of the game.dart file.
+  // It manages wordle logic, and is outside the scope of this tutorial.
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          for (final guess in _game.guesses)
+            Row(
+              spacing: 5.0,
+              children: [
+                // to do after sometime
+                for (var letter in guess) Tile(letter.char, letter.type)
+                
+              ],
+            ),
+            GuessInput(
+            onSubmitGuess: (guess) {
+              // TODO, handle guess
+              print(guess); // Temporary
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GuessInput extends StatelessWidget {
+  GuessInput({super.key, required this.onSubmitGuess});
+
+  final void Function(String) onSubmitGuess;
+  //New
+  final TextEditingController _textEditingController = TextEditingController();
+   final FocusNode _focusNode = FocusNode(); // NEW
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              maxLength: 5,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(35)),
+                ),
+              ),
+              controller: _textEditingController, // NEW
+              autofocus: true, // NEW
+              focusNode: _focusNode, // NEW
+               onSubmitted: (input) {
+                // NEW
+                print(_textEditingController.text); // Temporary
+                onSubmitGuess(_textEditingController.text.trim());
+                _textEditingController.clear(); // NEW
+                _focusNode.requestFocus(); // NEW
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
